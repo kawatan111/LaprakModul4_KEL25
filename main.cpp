b70581bdfc5490e060c4c54cd5324e2310b9ca5b
Tederby <98989443+Tederby@users.noreply.github.com>
2024-10-01 17:52:56 +0700

    Commit 1 oleh M. Afiffatin

--- main.cpp
+++ main.cpp
@@ -27,50 +27,20 @@ int indexjajanan[5][4] =
 int idjajanan,total=0;
 vector<int> beli;
 
-int main()
-{
-    int n1,n2,lagi;bool equal = false, as = true;
-    cout << setw (90)<< "Selamat Datang di Toko kami silahkan pilih barang yang anda inginkan" << endl;
-    showProduct();
-    while (as) {
-        equal = false;
-        cout<< "Masukan id barang yang anda inginkan(ketik 0 jika tidak jadi)"<< endl;
-        cin >> idjajanan;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (idjajanan == indexjajanan[i][j]) {      //Mengecek apakah idnya valid
-                    equal = true;
-                    beli.push_back(idjajanan);              //jika valid akan dimasukan ke nota
-                    break;                                  //Jika Valid maka akan keluar dari loop pengecekan
-                }else if(idjajanan == 0) {                  //Jika pelanggan tidak jadi beli
-                    as = false;                             //maka langsung keluar loop
-                    break;
-                }
-            }
-            if (equal) {
-                break;                                      //untuk keluar loop
-            }
+void nota() {//Menampilkan Nota
+    int d1,d2;
+    for (int i = 0; i < beli.size(); i++) {
+        d1 = beli[i]/10;
+        d2 = beli[i]%10;
+        cout << i+1 << ". ";
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k < 4; k++) {
+                cout << jajanan[d1-1][d2-1] << " " << hjajanan[d1-1][d2-1]<<endl;
+                total += hjajanan[d1-1][d2-1];
+                break;
+            }break;
         }
-        n1 = idjajanan/10;                                  //untuk mensplit integer
-        n2 = idjajanan%10;
-        if(as) {
-            if (equal) {
-                cout << "Selamat anda telah membeli barang : " << jajanan[n1-1][n2-1] << endl; //Menampilkan barang yang di beli
-            }else{
-                cout<<"Tidak Palid, masukan ulang kode"<<endl; // Menampilkan id barang tidak valid
-            }
-            if(equal) {
-                cout<< "Apakah anda ingin beli lagi??(1 iya 0 tidak)"<< endl;
-                cin >> lagi;
-                if(lagi != 1) {
-                    break;
-                }
-            }
-        }
-
     }
-    nota();                                                 //menampilkan nota
-    cout<< "Terima Kasih Telah Berbelanja di Toko kami"<< endl;
-    return 0;
+    for (int i = 0; i < 25; i++) {cout<<"------";}cout<<endl;
+    cout<<"Total belanja kamu adalah = "<<total <<endl;
 }
-
